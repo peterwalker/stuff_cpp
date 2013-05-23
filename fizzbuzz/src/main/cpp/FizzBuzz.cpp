3940ada5e3aae82770264203672325dfa7dbea2f
Peter Walker <peterbwalker@me.com>
2013-05-23 13:45:31 -0400

    more

---
+++ fizzbuzz/src/main/cpp/FizzBuzz.cpp
@@ -0,0 +1,17 @@
+/*
+ * FizzBuzz.cpp
+ *
+ *  Created on: May 23, 2013
+ *      Author: localdad
+ */
+
+#include <FizzBuzz.h>
+
+FizzBuzz::FizzBuzz() {
+	// TODO Auto-generated constructor stub
+
+}
+FizzBuzz::~FizzBuzz() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ fizzbuzz/src/main/headers/FizzBuzz.h
@@ -0,0 +1,17 @@
+/*
+ * FizzBuzz.h
+ *
+ *  Created on: May 23, 2013
+ *      Author: localdad
+ */
+
+#ifndef FIZZBUZZ_H_
+#define FIZZBUZZ_H_
+
+class FizzBuzz {
+public:
+	FizzBuzz();
+	virtual ~FizzBuzz();
+};
+
+#endif /* FIZZBUZZ_H_ */
